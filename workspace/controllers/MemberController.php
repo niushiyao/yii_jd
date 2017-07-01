f86fcbd4134f9b07830ce937764cfa8761c8f220
1167531 <niushiyaohero@163.com>
2017-07-01 14:13:48 +0800

    完成了qq登录

--- workspace/controllers/MemberController.php
+++ workspace/controllers/MemberController.php
@@ -63,6 +63,59 @@
             return $this->render('auth',['model' => $model]);
         }
        
-       
-       
+       /**
+        * qq登录
+        */
+        public function actionQqlogin()
+        {
+            require_once("../vendor/qqlogin/qqConnectAPI.php");
+            $qc = new \QC();
+            $qc->qq_login();
+        }
+        
+        /**
+         * QQ回调函数
+         */
+         public function actionQqcallback()
+         {
+             require_once("../vendor/qqlogin/qqConnectAPI.php");
+             $auth = new \OAuth();
+             $accessToken = $auth->qq_callback();
+             $openid = $auth->get_openid();
+             $qc = new \QC($accessToken,$openid);
+             $userinfo = $qc->get_user_info();
+             $session = Yii::$app->session();
+             $session['userinfo'] = $userinfo;
+             $session['openid'] = $openid;
+             if(User::find()->where('openid = :openid',[':openid' => $openid])->one())
+             {
+                 $session['loginname'] = $userinfo['nickname'];
+                 $session['isLogin'] = 1;
+                 return $this->redirect(['member/qqreg']);
+             }
+             return $this->redirect(['member/qqreg']);
+         }
+        
+        /**
+         * QQ登录注册
+         */
+         public function actionQqreg()
+         {
+             $this->layout = 'layout2';
+             $model = new User;
+             if(Yii::$app->request->isPost)
+             {
+                 $post = Yii::$app->request->post();
+                 $session = Yii::$app->session;
+                 $post['User']['openid'] = $session['openid'];
+                 if($model->reg($post,'qqreg'))
+                 {
+                     $session['loginname'] = $session['userinfo']['nickname'];
+                     $session['isLogin'] = 1;
+                     return $this->redirect(['index/index']);
+                 }
+             }
+             return $this->render('qqreg', ['model' => $model]);
+         }
+     
  }
--- workspace/views/member/auth.php
+++ workspace/views/member/auth.php
@@ -15,10 +15,10 @@ use yii\helpers\Html;
 					<div class="social-auth-buttons">
 						<div class="row">
 							<div class="col-md-6">
-								<button class="btn-block btn-lg btn btn-facebook"><i class="fa fa-qq"></i> 使用QQ账号登录</button>
+								<button id="login_qq" class="btn-block btn-lg btn btn-facebook"><i class="fa fa-qq"></i> 使用QQ账号登录</button>
 							</div>
 							<div class="col-md-6">
-								<button class="btn-block btn-lg btn btn-twitter"><i class="fa fa-weibo"></i> 使用新浪微博账号登录</button>
+								<button id="login_weibo" class="btn-block btn-lg btn btn-twitter"><i class="fa fa-weibo"></i> 使用新浪微博账号登录</button>
 							</div>
 						</div>
 					</div>
@@ -94,4 +94,10 @@ use yii\helpers\Html;
 		</div><!-- /.row -->
 	</div><!-- /.container -->
 </main><!-- /.authentication -->
+<script>
+    var qqbtn = document.getElementById("login_qq");
+    qqbtn.onclick = function(){
+        window.location.href="<?php echo yii\helpers\Url::to(['member/qqlogin']) ?>";
+    }
+</script>
 <!-- ========================================= MAIN : END ========================================= -->		<!-- ============================================================= FOOTER ============================================================= -->
